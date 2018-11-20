c56b63d4a672949e450ad1f7a3235d92ef6f1cac
Afia Udofia <afia@awltd.com>
2018-11-20 12:27:29 +0100

    added date format

--- src/components/mainbody/TweetColumn.js
+++ src/components/mainbody/TweetColumn.js
@@ -1,37 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import dateformat from 'dateformat'
 
-export default class TweetColumn extends Component {
+class TweetColumn extends Component {
   constructor(props) {
     super(props);
     this.state = {
       tweets: []
     };
-    
   }
-    
-  componentWillMount() {
+
+  getTweets() {
     let count = 30;
     let screen_name = this.props.accountName;
-    fetch("http://localhost:7890/1.1/statuses/user_timeline.json?count=" + count + "&screen_name=" + screen_name )
-    .then(response => response.json())
-    .then(tweets => this.setState({ tweets }));
+    let baseUrl = `http://localhost:7890/1.1/statuses/user_timeline.json?count=${count}&screen_name=${screen_name}`;
+    return axios.get(baseUrl).then(res => {
+      const tweets = res.data;
+      this.setState({ tweets });
+    });
   }
-  
+
+  componentWillMount() {
+    this.getTweets();
+  }
+
+  // componentDidMount() {
+  //   this.getTweets()
+  // }
+
   render() {
     console.log(this.state.tweets);
     return (
-      <div className="col-md-4 m-0 p-1">
-      <h2 className="lead">Tweets from { this.props.accountName }</h2>
-      {this.state.tweets.map((tweet, index) => (
-        <div className="tweetbox p-2" key={tweet.id}>
-              <a href="#">
-              <div className="tweet_text">{tweet.text}</div>
-              {/* <div className="tweet_url">{tweet.entities[0]}</div> */}
-              <div className="tweet_date">{tweet.created_at.toLocaleDateString()}</div>
-              </a>
-        </div>
+      <div className="col-md-4 p-1">
+        <h2 className="lead">Tweets from {this.props.accountName}</h2>
+        {this.state.tweets.map((tweet, index) => (
+          <div className="tweetbox p-2" key={tweet.id}>
+            <div className="tweet_text">{tweet.text}</div>
+            {/* <div className="tweet_url">{tweet.entities.urls.url}</div> */}
+            <div className="tweet_date">{dateformat(tweet.created_at, "mmm dS, yyyy, h:MM TT")}</div>
+          </div>
         ))}
       </div>
-    )
+    );
   }
 }
+export default TweetColumn;
